be0b9c5bd986694bae37f6e553eb12df41c91147
ferman <sizlanan@gmail.com>
2021-04-23 20:56:03 +0300

    meta-description

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- Required Meta Tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Rhoncus Çiğköfte Adıyaman'dan gelen eşsiz lezzetleri ve temiz işletme anlayışıyla İzmir-Bornova Küçükpark da hizmet vermektedir.">
 
     <!-- Page Title -->
     <title>@yield('title')</title>
@@ -22,23 +24,24 @@
     <link rel="stylesheet" href="{{asset('assets/sweet-alert/sweetalert2.css')}}">
     @yield('css')
 </head>
+
 <body>
     <!-- Preloader Starts -->
 
-        @include('layouts.preloader')
+    @include('layouts.preloader')
 
     <!-- Preloader End -->
 
     <!-- Header Area Starts -->
 
-        @include('layouts.menu')
+    @include('layouts.menu')
 
     <!-- Header Area End -->
 
 
     <!-- Content Area Starts -->
 
-        @yield('content')
+    @yield('content')
 
     <!-- Content Area End -->
 
@@ -90,10 +93,14 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-7 col-md-6">
-                        <span><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> <a href="{{Route('index')}}">Rhoncus Çiğköfte</a> Tüm hakları saklıdır.
-<br> &nbsp;&nbsp;&nbsp;&nbsp; Web tasarım<i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="tel:05379140983" target="">Ferman SIZLANAN - Selluka Reklam</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></span>
+                        <span>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;<script>
+                                document.write(new Date().getFullYear());
+                            </script> <a href="{{Route('index')}}">Rhoncus Çiğköfte</a> Tüm hakları saklıdır.
+                            <br> &nbsp;&nbsp;&nbsp;&nbsp; Web tasarım<i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="tel:05379140983" target="">Ferman SIZLANAN - Selluka Reklam</a>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </span>
                     </div>
                     <div class="col-lg-5 col-md-6">
                         <div class="social-icons">
@@ -116,7 +123,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> <a hr
 
     <!-- Javascript -->
     <script src="{{asset('assets/js/vendor/jquery-2.2.4.min.js')}}"></script>
-	<script src="{{asset('assets/js/vendor/bootstrap-4.1.3.min.js')}}"></script>
+    <script src="{{asset('assets/js/vendor/bootstrap-4.1.3.min.js')}}"></script>
     <script src="{{asset('assets/js/vendor/wow.min.js')}}"></script>
     <script src="{{asset('assets/js/vendor/owl-carousel.min.js')}}"></script>
     <script src="{{asset('assets/js/vendor/jquery.datetimepicker.full.min.js')}}"></script>
@@ -126,4 +133,5 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> <a hr
     <script src="{{asset('assets/sweet-alert/sweetalert2.all.js')}}"></script>
     @yield('js')
 </body>
+
 </html>
